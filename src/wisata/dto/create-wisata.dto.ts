37d919de7f76fca145423dd7e8440c2d583f1a7d
Farhan Franaka <farhanfranaka0505@gmail.com>
2024-07-09 10:39:54 +0800

    fix dto wisata

--- src/wisata/dto/create-wisata.dto.ts
+++ src/wisata/dto/create-wisata.dto.ts
@@ -1,10 +1,45 @@
+import {
+  IsString,
+  IsNotEmpty,
+  IsOptional,
+  IsNumber,
+  IsArray,
+  IsEmail,
+  IsUrl,
+  IsLatitude,
+  IsLongitude,
+} from 'class-validator';
+
 export class CreateWisataDto {
+  @IsString()
+  @IsNotEmpty()
   readonly name: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly category: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly description: string;
+
+  @IsString()
+  @IsNotEmpty()
   readonly address: string;
+
+  @IsNumber()
+  @IsLatitude()
   readonly latitude: number;
+
+  @IsNumber()
+  @IsLongitude()
   readonly longitude: number;
-  readonly image: string;
+
+  @IsArray()
+  @IsNotEmpty()
+  readonly image: string[];
+
+  @IsString()
+  @IsNotEmpty()
   readonly desa: string;
 }
